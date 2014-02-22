a45863a2d75cadb06ab655757a51cf6ec441f594
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2014-02-22 16:07:50 +0100

    Shadowing the regions depending on their 2nd column value

--- js/euskadi_gc.js
+++ js/euskadi_gc.js
@@ -1,13 +1,14 @@
 function loadMap(data, options) {
     $.get("images/euskadi.svg", {}, function(img) {
-	console.log('about to use the svg');
+	console.log('about to show the svg');
 	$("#svgmap").html(img);
-	//$("#bilbao").css("fill","#ff3333");
 	
 	var rowInds = data.getSortedRows([{column: 1}]);
 	for (var i = 0; i < rowInds.length; i++) {
 	  createTooltip(data, rowInds[i]);
+	  setBackgroundColors(data, rowInds[i]);
 	}
+	updateLegend(data);
 	
 	var show_id = "#";
 	if(options.resolution=="municipality") {
@@ -27,12 +28,15 @@ function loadMap(data, options) {
     },"text");
 }
 
+function getElementId(data, rowNumber) {
+  return "#" + data.getValue(rowNumber, 0).toLowerCase();
+}
+
 function createTooltip(data, rowNumber) {
-    var t = data.getValue(rowNumber, 0);
-    var idreg = "#" + t.toLowerCase();
-    createTooltipText( t.toLowerCase(), data, rowNumber );
+    var idreg = getElementId(data, rowNumber);
+    createTooltipText( idreg.substring(1), data, rowNumber );
     
-    $(idreg).attr("title", t); // Needed for tooltip
+    $(idreg).attr("title", idreg.substring(1)); // Needed for tooltip
     $(idreg).tooltip({
 	content:  function() {
 		      var currentId = $(this).attr("id");
@@ -50,14 +54,44 @@ function createTooltipText(forId, data, rowNumber) {
 }
 
 function getTooltipText(data, rowNumber) {
-    var t = data.getValue(rowNumber, 0);
-    var ret = "<b>" + t + "</b>";
-    
-    
+    var ret = "<b>" + data.getValue(rowNumber, 0) + "</b>";    
     for (var i=1; i<data.getNumberOfColumns(); i++) {
       var label = data.getColumnLabel(i);
       var v = data.getValue(rowNumber, i);
       ret += "<br/>" + label + ": " + v;
     }
     return ret;
+}
+
+function setBackgroundColors(data, rowNumber) {
+    //$("#bilbao").css("fill","#ff3333");
+    var col = "#" + getProportionalRGBs( data.getValue(rowNumber, 1), data.getColumnRange(1), "#efe6dc" , "#109618" );
+    var idreg = getElementId(data, rowNumber);
+    $(idreg).css("fill", col);
+}
+
+function getProportionalRGBs(current, range, minColor, maxColor) {
+    var r = getProportionalColor(current, range, minColor.substring(1,3), maxColor.substring(1,3));
+    var g = getProportionalColor(current, range, minColor.substring(3,5), maxColor.substring(3,5));
+    var b = getProportionalColor(current, range, minColor.substring(5,7), maxColor.substring(5,7));
+    return r + g + b;
+}
+
+function d2h(d) {
+    return (+d).toString(16).toUpperCase();
+}
+
+function getProportionalColor(current, range, minColor, maxColor) {
+    // avoid "#"s and convert to decimals
+    var decMinCol = parseInt(minColor, 16); 
+    var decMaxCol = parseInt(maxColor, 16);
+    
+    var decCol = ( (current-range.min) * (decMaxCol - decMinCol) / (range.max - range.min) ) + decMinCol;
+    //JavaScript only has a Number type
+    //If you want to format the number as a string with two digits after the decimal point use:
+    return  d2h(decCol.toFixed(0));
+}
+
+function updateLegend(data) {
+    var range = data.getColumnRange(1);
 }
